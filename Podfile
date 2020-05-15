7a7bb3bdc79be960303fab9c9b772fba77891c72
Santiago Bustamante <busta117@gmail.com>
2020-05-15 14:49:41 -0500

    first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'PokeDex' do
+
+  pod 'SVProgressHUD'
+  pod 'RxSwift'
+  pod 'AlamofireObjectMapper'
+
+end
---
+++ PokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeDex
+//
+//  Created by Santiago Bustamante on 15/05/20.
+//  Copyright © 2020 Santiago Bustamante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
